7dd88013e3ae12384358af3767d18cf777d48ab3
Mahad12345-67 <144786599+Mahad12345-67@users.noreply.github.com>
2024-05-12 22:09:15 +0200

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import SearchForm from './SearchForm';
+import RecipeCard from './RecipeCard';
+import RecipeDetails from './RecipeDetails';
+
+function App() {
+  const [query, setQuery] = useState('');
+  const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+
+  const handleChange = event => {
+    setQuery(event.target.value);
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+      );
+      const data = await response.json();
+      setRecipes(data.meals);
+      setSelectedRecipe(null);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleGoBack = () => {
+    setRecipes([]);
+    setSelectedRecipe(null);
+  };
+
+  const handleSelectRecipe = recipe => {
+    setSelectedRecipe(recipe);
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Mahad's Foodmenu</h1>
+      <blockquote>Real food doesn't have ingredients, real food is ingredients.
+          <cite> - Mahad Farah</cite>
+      </blockquote>
+      {!selectedRecipe && (
+        <SearchForm 
+          query={query} 
+          onChange={handleChange} 
+          onSubmit={handleSubmit}
+        />
+      )}
+      <div className="row">
+        {recipes &&
+          recipes.map(recipe => (
+            <RecipeCard 
+              key={recipe.idMeal} 
+              recipe={recipe} 
+              onSelectRecipe={handleSelectRecipe}
+              selectedRecipe={selectedRecipe}
+            />
+          ))}
+      </div>
+      {selectedRecipe && (
+        <RecipeDetails 
+          recipe={selectedRecipe} 
+          onGoBack={handleGoBack}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
